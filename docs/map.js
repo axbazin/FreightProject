a8fee910f05d6ede29540da8c9ed102fe586488d
John Doe <johndoe@example.com>
2018-01-03 15:44:12 +0100

    integration des données à la map (map.js) + mise en forme index.htlm avec les otpions

--- docs/map.js
+++ docs/map.js
@@ -70,23 +70,61 @@ function mapDrawing(){
 		.attr('height',config.height)
 		.attr("id","view");
 
+	var tooltip = d3.select('body').append('div')
+            .attr('class', 'tooltip');
+
+	var colours = d3.scaleLinear()
+		.range(['#ffd59b','#8b0000'])
+	//d3.scaleQuantile().range(['#ffffe0','#ffd59b','#ffa474','#f47461','#db4551','#b81b34','#8b0000']);
+
+
 	var g2r = new geo2rect.draw();
 
-	d3.json('eurovis-countries-simplified.geojson', function(err, data){
-		var geojson = geo2rect.compute(data);
-		g2r.config = config;
-		g2r.data = geojson;
-		g2r.svg = svg.append('g');
-		g2r.draw();
+
+	d3.json("exports.json", function(err,data){
+
+		var yearsArray = Object.keys(data);
+		var currentYear = yearsArray[0];
+
 		
-		console.log(svg.selectAll("path"));
+		colours.domain([
+			0,20000
+			// d3.min(data, function(d) { return d; }),
+			// d3.max(data, function(d) { return d; })
+			]);
 		
+		d3.json('eurovis-countries-simplified.geojson', function(err, json){
+
+			
+			var geojson = geo2rect.compute(json);
+			g2r.config = config;
+			g2r.data = geojson;
+			g2r.svg = svg.append('g');
+			g2r.draw();
+
+
+			for(var i=0; i<Object.keys(data[currentYear]).length ; i++){
+				d3.select(".id-"+ Object.keys(data[currentYear])[i])
+					.style("fill", colours(Object.values(data[currentYear])[i].TOTAL.TOTAL));
+				//console.log(Object.values(d[currentYear])[i].TOTAL.TOTAL);
+			}
+		});
+
+      	d3.select("#slider").on("input", function() {
+			d3.select('#year').html(this.value);
+			updateViz(this.value, data,colours);
+		});
+
+		d3.select()
 	});
+
+
+
 
 	d3.select('#toggle').on('click', function(){
 		g2r.toggle();
 		g2r.draw();
-		console.log(g2r._mode);
+		//console.log(g2r._mode);
 		/*for(let c in trans_data){
 			if(g2r._mode === "rect"){
 				d3.select("#" + trans_data[c].ISO3).attr("fill","Grey");
@@ -96,5 +134,31 @@ function mapDrawing(){
 		}*/
 	});
 
-	
-};
+}
+
+function HandleMouseOn(d){
+	console.log(d.key);
+  	// tooltip.classed('hiden',false)
+   //      .html(d.key)
+   //      .style("left", (d3.event.pageX) + "px")		
+   //      .style("top", (d3.event.pageY - 28) + "px");
+}
+
+function HandleMouseOut(){
+  	// tooltip.classed('hiden',true);
+}
+
+
+function updateViz(currentYear, data, colours){
+	//color la map avec les données pour l'année currentYear
+		for(var i=0 ; i<Object.keys(data[currentYear]).length ; i++){
+			d3.select('.id-'+ Object.keys(data[currentYear])[i])
+				.style("fill", function(){
+					if(Object.values(data[currentYear])[i].TOTAL.TOTAL){
+						return colours(Object.values(data[currentYear])[i].TOTAL.TOTAL)
+					}else{
+						return "#ccc"
+					}
+				});
+		}
+}
